02eef1502ec19e44c002df76b675d4ed3db12ba7
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-24 21:37:17 -0800

    completed pastry tests

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -1,4 +1,36 @@
-public class Pastry
+using System.Security.Cryptography.X509Certificates;
+
+namespace Bakery.Models
 {
-  
+
+  public class Pastry
+  {
+    public int Order { get; set; }
+    public Pastry(int quantity)
+    {
+      Order = quantity;
+    }
+
+    public int CalcPrice()
+    {
+      if (Order < 1)
+      {
+        return 0;
+      }
+      else
+      {
+        if (Order % 4 == 0)
+        {
+          int dealOrder = (Order - (Order /  4));
+          int breadTotalCost = dealOrder * 2;
+          return breadTotalCost;
+        }
+        else
+        {
+          return Order * 2;
+        }
+      }
+    }
+  }
+
 }
