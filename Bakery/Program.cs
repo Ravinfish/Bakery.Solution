5a4d8bb23f4ce7b3382ce5727d4804545b43c598
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-26 10:35:44 -0800

    update welcome banner

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,6 +8,10 @@ namespace Bakery
     static void Main()
     {
       Console.BackgroundColor = ConsoleColor.Magenta;
+      Console.Clear();
+      Console.WriteLine(Bakery.UserInterfaceModels.WelcomeBanner.Welcome);
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to Roll & Rise Bakery!");
     }
   }
 }
--- Bakery/UserInterfaceModels/Banners.cs
+++ Bakery/UserInterfaceModels/Banners.cs
@@ -3,34 +3,26 @@ namespace Bakery.UserInterfaceModels
   class WelcomeBanner
   {
     public static string Welcome { get; } = @"
- ████    ███    █████    █████    ████    ████
- █  ██  █  ██   █       █       █  ██   █  ██
- ███   ███     █████   ████     ███   ████
- █  ██  █  ██   █       █       █  ██   █  ██
- ████   ████    █████    █████    ████   ████
-                                                   
- ████   ███    █████    █████    ████    ████
- █  ██  █  ██   █       █       █  ██   █  ██
- ███   ███     █████   ████     ███   ████
- █  ██  █  ██   █       █       █  ██   █  ██
- ████   ████    █████    █████    ████   ████
+ WWWW   WWWW  EEEEE  L      CCCCC OOOO  M   M  EEEEE
+ WWWW   WWWW  E      L      C     O  O  MM MM  E
+ WWWW W WWWW  EEEE   L      C     O  O  M M M  EEEE
+ WWWW W WWWW  E      L      C     O  O  M   M  E
+  WW   WW     EEEEE  LLLLL  CCCCC OOOO  M   M  EEEEE
         ";
   }
 
   class GoodbyeBanner
   {
     public static string Bye { get; } = @"
- ██   ██  ███   ████   ███   ██  ████
- ██   ██ █  ██  █  ██ █  ██  ██ █  ██
- ███████ ██████  ███  ███   ██ ████
- ██   ██ █  ██  █  ██ █  ██  ██ █  ██
- ██   ██ █  ██  ███   ██   ██ ██  ██
-                                       
- ██   ██  ███   ████   ███   ██  ████
- ██   ██ █  ██  █  ██ █  ██  ██ █  ██
- ███████ ██████  ███  ███   ██ ████
- ██   ██ █  ██  █  ██ █  ██  ██ █  ██
- ██   ██ █  ██  ███   ██   ██ ██  ██
+ BBBBBB  YY   YY  EEEEEE
+ BBBBBB  YY   YY  EEEEEE
+ BB   BB YY   YY  EE
+ BB   BB  YY YY   EE
+ BBBBBB     YY    EEEEEE
+ BB    BB   YY    EE
+ BB    BB   YY    EE 
+ BBBBBB     YY    EEEEEE
+ BBBBBB     YY    EEEEEE
         ";
   }
 
