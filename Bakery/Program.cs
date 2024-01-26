0d4f5364efbfa255d7e2a405295efbb7d3a74347
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-01-26 10:49:09 -0800

    Setup welcome and menu in console

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,8 +10,14 @@ namespace Bakery
       Console.BackgroundColor = ConsoleColor.Magenta;
       Console.Clear();
       Console.WriteLine(Bakery.UserInterfaceModels.WelcomeBanner.Welcome);
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("");
       Console.WriteLine("Welcome to Roll & Rise Bakery!");
+      Console.WriteLine("");
+      Console.WriteLine("Here is the menu: \n* Bread: $5 per loaf \n* Pastries: $2 each");
+      Console.WriteLine("");
+      Console.WriteLine("Today's special: \n* Bread: Buy 2, get one FREE! \n* Pastries: Buy 3, get one FREE!");
+      // PlaceOrder();
     }
   }
 }
